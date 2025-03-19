e19397decd07014252714c9b52cadc2170b77bbd
Vinit <vinitnagar56@gmail.com>
2025-03-19 14:40:34 +0530

    added fav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,10 +8,10 @@ function Navbar({ setselectfxn, setisopen, isopen }: { setselectfxn: (option: st
 <nav className="flex justify-between items-center mb-2 py-2 px-4 bg-[#1a1a22] border border-[#2d2d3a] rounded-md shadow-lg">
   <div className="flex gap-4 justify-between w-full items-center">
     <div
-      onClick={() => setisopen(!isopen)}
+      onClick={() => {setisopen(!isopen) ; setselectfxn('projects')}}
       className={`font-bold text-4xl text-gray-500 transition-transform duration-300 ${isopen ? 'hover:-rotate-90' : 'hover:rotate-90'} tracking-tight`}
     >
-      <RxVercelLogo size={30} title="Not Illuminati 😆" />
+      <RxVercelLogo size={30}/>
     </div>
     
     <div className="md:hidden flex items-center text-gray-500 cursor-pointer"
