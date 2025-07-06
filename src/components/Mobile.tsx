512df601c3ef7edd48dee1bbc0e4c5dab98ce9fe
Vinit <vinitnagar56@gmail.com>
2025-07-06 15:56:14 +0530

    fix

--- src/components/Mobile.tsx
+++ src/components/Mobile.tsx
@@ -134,9 +134,10 @@ function Mobile({ onOutOfView }: { onOutOfView?: (isOutOfView: boolean) => void 
       </div>
 
       <div className="mb-3">
-        <h3 className="font-semibold mb-1 text-sm">Currently Into.</h3>
-        <p className="text-gray-400 text-sm">
-          Currently exploring @modelcontextProtocol, @Ai/ML , and @video_streaming — while grinding for semester exams.
+
+        <h3 className="font-semibold mb-1">Currently Into.</h3>
+        <p className="text-gray-300">
+          Currently exploring @modelcontextProtocol, @Ai/ML , and @video_streaming
         </p>
       </div>
     </div>
